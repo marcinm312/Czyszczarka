d4117ea5c94b7a21d5ef08c0b5aae6e1fa15275a
marcinm312 <marcinm312@interia.pl>
2019-03-13 21:44:16 +0100

    Liczba dni z pliku

--- Usuwarka/Program.cs
+++ Usuwarka/Program.cs
@@ -11,8 +11,22 @@ namespace Usuwarka
         static void Main(string[] args)
         {
             string scriptStartTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            int days = 1000000;
             try
             {
+                //Pobranie liczby dni z pliku
+                StreamReader myReader2 = new StreamReader("Days.txt");
+                string line2 = "";
+                while (line2 != null)
+                {
+                    line2 = myReader2.ReadLine();
+                    if (line2 != null)
+                    {
+                        days = Int32.Parse(line2);
+                    }
+                }
+
+                //Odczytywanie z pliku tekstowego ścieżek i usuwanie plików
                 StreamReader myReader = new StreamReader("Directories.txt");
                 string line = "";
 
@@ -21,41 +35,40 @@ namespace Usuwarka
                     line = myReader.ReadLine();
                     if (line != null)
                     {
-                        //Console.WriteLine(line);
                         string[] files = Directory.GetFiles(line);
-
                         foreach (string file in files)
                         {
                             Console.WriteLine(file);
                             FileInfo fi = new FileInfo(file);
-                            if (fi.LastWriteTime < DateTime.Now.AddDays(-30))
+                            //Sprawdzam czy plik jest starszy niż ilość dni zdefiniowana w pliku
+                            if (fi.LastWriteTime < DateTime.Now.AddDays(-days))
                             {
                                 try
                                 {
+                                    //Zapisuję do logów ścieżkę usuniętego pliku
                                     FileStream fs = new FileStream(scriptStartTime + "_LogFile.txt", FileMode.Append, FileAccess.Write);
                                     StreamWriter sw = new StreamWriter(fs);
-                                    string logline = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " " + file;
-                                    Console.WriteLine(logline);
+                                    string logline = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " Data pliku: " + fi.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss") + " " + file;
                                     sw.WriteLine(logline);
                                     sw.Close();
-                                } 
+                                }
                                 catch (Exception e1)
                                 {
                                     Console.WriteLine("Something didn't quite work correctly: {0}", e1.Message);
                                 }
+                                //Usunięcie pliku
                                 fi.Delete();
                             }
                         }
                     }
                 }
-                
                 myReader.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Something didn't quite work correctly: {0}", e.Message);
             }
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
